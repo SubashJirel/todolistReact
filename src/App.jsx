98c76eecc89bb204001afb67ff14fc84a26a4e37
Subash Jirel <subashjrl@gmail.com>
2024-10-12 21:23:05 +0545

    fixed the filtering functionality for projects

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import './App.css';
 import Projects from './components/Projects';
 import Sidebar from './components/Sidebar';
 import MainPanel from './components/MainPanel';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { isToday, isThisWeek, parseISO } from 'date-fns';
 
 function App() {
   const [projectsList, setProjectsList] = useState([
     {
       title: 'default Project',
+      projectIndex: 0,
       tasks: [
         {
           title: 'This is a sample task',
           description: 'some short description',
-          date: '2024-10-10',
+          date: '2024-11-11',
           projectIndex: 0,
           taskIndex: 0,
           completed: false,
@@ -21,7 +22,7 @@ function App() {
         {
           title: 'This is a completed task',
           description: 'some short description',
-          date: '2024-10-11',
+          date: '2024-11-11',
           projectIndex: 0,
           taskIndex: 1,
           completed: true,
@@ -30,6 +31,7 @@ function App() {
     },
     {
       title: 'Workout',
+      projectIndex: 1,
       tasks: [
         {
           title: 'Do regular home workout',
@@ -41,35 +43,95 @@ function App() {
         },
       ],
     },
+    {
+      title: 'Study Project',
+      projectIndex: 2,
+      tasks: [
+        {
+          title: 'Reading Novel',
+          description: 'Read read read',
+          date: '2024-10-12',
+          projectIndex: 2,
+          taskIndex: 0,
+          completed: false,
+        },
+        {
+          title: 'Study data warehose and data mining',
+          description: 'for machine learning and model training',
+          date: '2024-10-11',
+          projectIndex: 2,
+          taskIndex: 1,
+          completed: true,
+        },
+      ],
+    },
   ]);
   const [selectedView, setSelectedView] = useState('all'); // 'all', 'today', 'week', or project index
+  const [filteredTasks, setFilteredTasks] = useState([]);
 
-  const getFilteredTasks = () => {
-    let tasks = [];
-    projectsList.forEach((project) => {
-      project.tasks.forEach((task) => {
-        const taskDate = parseISO(task.date);
-        if (selectedView === 'all') {
-          tasks.push(task);
-        } else if (selectedView === 'today' && isToday(taskDate)) {
-          tasks.push(task);
-        } else if (selectedView === 'week' && isThisWeek(taskDate)) {
-          tasks.push(task);
-        } else if (
-          typeof selectedView === 'number' &&
-          selectedView === project.projectIndex
-        ) {
-          tasks.push(task);
-        }
+  // const getFilteredTasks = () => {
+  //   let tasks = [];
+  //   projectsList.forEach((project) => {
+  //     project.tasks.forEach((task) => {
+  //       const taskDate = parseISO(task.date);
+  //       // console.log(taskDate);
+  //       // console.log(typeof selectedView);
+  //       if (selectedView === 'all') {
+  //         tasks.push(task);
+  //       } else if (selectedView === 'today' && isToday(taskDate)) {
+  //         tasks.push(task);
+  //       } else if (selectedView === 'week' && isThisWeek(taskDate)) {
+  //         tasks.push(task);
+  //       } else if (
+  //         typeof selectedView === 'number' &&
+  //         selectedView === project.projectIndex
+  //       ) {
+  //         // tasks.push(task);
+  //         // console.log('this issssssssssssssssssssssssssss trueeeeeeeeeeee');
+
+  //         console.log(selectedView);
+  //         console.log(project.projectIndex);
+  //         console.log(tasks);
+  //       }
+  //     });
+  //   });
+  //   return tasks;
+  // };
+  // console.log(selectedView);
+
+  // if i didn't use useEffect hook here for flterTasks function, then i got undefined for task.projectIndex, intially then only i got number in later render
+  useEffect(() => {
+    const filterTasks = () => {
+      let tasks = [];
+      projectsList.forEach((project) => {
+        project.tasks.forEach((task) => {
+          const taskDate = parseISO(task.date);
+          if (selectedView === 'all') {
+            tasks.push(task);
+          } else if (selectedView === 'today' && isToday(taskDate)) {
+            tasks.push(task);
+          } else if (selectedView === 'week' && isThisWeek(taskDate)) {
+            tasks.push(task);
+          } else if (
+            typeof selectedView === 'number' &&
+            selectedView == task.projectIndex
+          ) {
+            // console.log('This is trueeee');
+            // console.log(selectedView);
+            // console.log(task.projectIndex);
+            tasks.push(task);
+          }
+        });
       });
-    });
-    return tasks;
-  };
+      setFilteredTasks(tasks);
+    };
 
+    filterTasks();
+  }, [selectedView, projectsList]); // Depend on selectedView and projectsList
   return (
     <div className="todolist-app">
       <Sidebar setSelectedView={setSelectedView} projectsList={projectsList} />
-      <MainPanel tasks={getFilteredTasks()} setProjectsList={setProjectsList} />
+      <MainPanel tasks={filteredTasks} setProjectsList={setProjectsList} />
     </div>
   );
 }
